43043371848db9102ed1a45b95b09bdeceee8136
Holuwaphemmy01 <Jacoboluwafemi2002@gmail.com>
2024-08-10 09:43:14 +0100

    Phase gate

--- gateOne/gtBank/BankAccount.java
+++ gateOne/gtBank/BankAccount.java
@@ -41,8 +41,16 @@ public static void main(String[] args){
 		Long number = random.nextLong(1000000000L, 9999999999L);
 		accountNumber.add(number);
 
-		System.out.print("\nEnter your first name and last name: ");
-		accountName.add(scanner.next()); 
+		System.out.print("\nEnter your first name: ");
+		String firstName = scanner.next();
+		
+		
+		System.out.print("\nEnter your last name:  ");
+		String lastName = scanner.next();
+		String fullName = firstName + " " + lastName;
+		accountName.add(fullName);
+		 
+
 		System.out.print("\nCreate your pin: ");
 		accountPin.add(scanner.next());
 
@@ -54,32 +62,45 @@ public static void main(String[] args){
 		accountBalance.add(scanner.nextDouble());
 
 
-		System.out.print("Enter 1 to go back to main menu or 0 to end: ");
+		System.out.print("Enter 1 to go back to main menu or 8 to end: ");
 		appOpening = scanner.nextInt();
 			
-	}
+	}	
 
 
 	if(appOpening == 2){
 			
-			System.out.print("\n Enter the receiver account number");
+			System.out.print("\n Enter the recipient account number: \n");
 			Long receiver = scanner.nextLong();
 			
-			System.out.print("\nHow much do you want to deposit: ");
+			System.out.print("\nHow much do you want to deposit: \n");
 			double account = scanner.nextLong();
-
+			
 			int index = accountNumber.indexOf(receiver);
 			if(index != -1){
+			String recieverName = accountName.get(index);
+			System.out.print("\nConfirm recipient name "+recieverName+" (Yes or No)\n");
+			String confirmName = scanner.next();
+			if(confirmName.equalsIgnoreCase("Yes")){
+
 			double count = accountBalance.get(index);
 			double counter = count + account;
 			accountBalance.set(index, counter);
 			System.out.print("\nTransfer successful.\n");
+							}
+			else
+			System.out.print("\nWrong account number entered.\n");
+			
+
 					}
 			
 			else 
 			System.out.print("Account number does exits.");
+			System.out.print("Enter 1 to go back to main menu or 8 to end: \n");
+		appOpening = scanner.nextInt();
 
 					}
+
 
 	if(appOpening == 3){
 			System.out.print("\nEnter your account number to withdraw: ");
@@ -87,8 +108,6 @@ public static void main(String[] args){
 
 			
 			int index = accountNumber.indexOf(withdrawalAccount);
-			//Long compareAccountNumber = accountNumber.get(index); 
-			//if (compareAccountNumber == withdrawalAccount){
 			if(index != -1){
 			  System.out.print("\nEnter amount to withdraw:  \n");
 			  double withdrawAmount = scanner.nextDouble();
@@ -98,22 +117,24 @@ public static void main(String[] args){
 					System.out.print("Enter your PIN: ");
 					String withdrawalPin = scanner.next();
 
-					int counterPin = accountPin.indexOf(withdrawalPin);
-					 if(counterPin != -1){
-				        double result =  count - withdrawAmount;
-					accountBalance.set(index, result);
-					System.out.print("\n Withdraw Successful");
+					String counterPin = accountPin.get(index);
+					 if(counterPin.equals(withdrawalPin)){
+				        	double result = count - withdrawAmount;
+						accountBalance.set(index, result);
+						System.out.print("\n Withdraw Successful.\n");
 					}
 				        else
-					System.out.print("\nWrong Pin!!! \n");
+						System.out.print("\nWrong Pin!!! \n");
 					}
 				  else 
-				  System.out.print("\nInsufficient Funds....\n");
+				  	System.out.print("\nInsufficient Funds....\n");
 				}
 			else 
-			    System.out.print("\nInvalid account number....\n");
+			   	 System.out.print("\nInvalid account number....\n");
 			
 			}
+
+		
 
 
 	if(appOpening == 4){
@@ -140,39 +161,91 @@ public static void main(String[] args){
 					
 
 	if(appOpening == 5){
-				System.out.print("\nEnter your account number: \n");
+				System.out.print("\nEnter your account number to login: \n");
 				Long transferAccount = scanner.nextLong();
-
 				int index = accountNumber.indexOf(transferAccount);
-				if(index != -1){
-					System.out.print("\nEnter your pin to login to your account: \n");
-					String loginPin = scanner.next();
-					
-					int count = accountPin.indexOf(loginPin);
-					if(count != -1){
-					System.out.print("\nEnter account number: ");
-					Long destinationAccount = scanner.nextLong();
-					System.out.print("Enter amount: ");
-					double transferAmount = scanner.nextDouble();
-					System.out.print("Enter PIN: ");
-					String pin = scanner.next();
+				scanner.nextLine();
+				if (index != -1){
+					System.out.print("\nEnter your account pin: \n");
+					String loginPin = scanner.nextLine();
+					String newLoginPin = accountPin.get(index);
+					if(newLoginPin.equalsIgnoreCase(loginPin)){
+						System.out.print("\nEnter 1 to transfer to gtBank or Enter 2 to transfer to other banks \n");
+						int bankSelection = scanner.nextInt();
+						if(bankSelection == 1){
+							System.out.print("\nEnter recipient account number: ");
+							Long recipientAccount = scanner.nextLong();
+							scanner.nextLine();
+							int counter = accountNumber.indexOf(recipientAccount);
 
-					int counter = accountPin.indexOf(pin);
-					if(counter == count){
-						System.out.print("\nTransfer successful.........");	
-						}
-					else 
-						System.out.print("\nWrong PIN.");
+							if(counter != -1){
+								
+							   System.out.print("\nEnter amount to transfer: ");	
+							   double transferAmount = scanner.nextDouble();
+							  double initialBalance = accountBalance.get(index);
+								scanner.nextLine();
+							     if(initialBalance >= transferAmount){
+								   String confirmRecipientName = accountName.get(counter);
+									System.out.print("\nConfirm recipient name(Yes or No)  "+confirmRecipientName+":  ");
+									String confirmName = scanner.nextLine();
+									scanner.nextLine();
+									if(confirmName.equalsIgnoreCase("Yes")){
+										System.out.print("\nEnter your account PIN to confirm transfer.\n");
+										String confirmTransferPin = scanner.nextLine();
+										if(confirmTransferPin.equalsIgnoreCase(newLoginPin)){
+											double newBalance = initialBalance - transferAmount;
+											accountBalance.set(index, newBalance);
+											double recipientBalance = accountBalance.get(counter);
+											double finalRecipient = recipientBalance + transferAmount;
+											accountBalance.set(counter, finalRecipient);
+											System.out.print("\nTransfer succeeful.\n");
+											}
+										else
+										System.out.print("\nYou have entered a wrong PIN.\n");
+										}
+									else
+									   System.out.println("\nCheck the account number again. ");
+									}
+							     else 
+								System.out.print("\nInsufficient funds......\n");
+									}
+							else 
+							   System.out.print("\nYou have entered a wrong account number.\n");
+ 
+								}
+						else{
+							System.out.print("\nEnter recipient account number: ");
+							Long recipientAccount = scanner.nextLong();
+							System.out.print("\nEnter amount to transfer: ");	
+							double transferAmount = scanner.nextDouble();
+								
+							  double initialBalance = accountBalance.get(index);
+							if(initialBalance >= transferAmount){
+								scanner.nextLine();
+								System.out.print("\nEnter your account pin to confirm transfer: ");
+								String confirmTransfer = scanner.nextLine();
+															
+								if(confirmTransfer.equalsIgnoreCase(loginPin)){
+								double finalBalance = initialBalance - transferAmount;
+								accountBalance.set(index, finalBalance);
+								System.out.print("\nTransfer successful....\n");
+								}
+								else
+								System.out.print("\nYou entered a wrong pin. Please try again.\n");
+								}
+							else
+							 System.out.print("\nInsufficient funds.\n");
+								}
+							
 							}
 					else 
-						System.out.print("\nWrong PIN");
-					}
-					else 
-						System.out.print("\nAccount number not valid.");
+						System.out.print("\nWrong pin entered....\n");
+					}	
+				else		
+				System.out.print("\nAccount number does not exist.\n"); 		
+} 
 
-					}
-
-
+		
 	if(appOpening == 6){
 				System.out.print("\nEnter your account number: ");
 				Long changePinAccount = scanner.nextLong();
@@ -189,9 +262,9 @@ public static void main(String[] args){
 						 	if(count != -1){
 								System.out.print("\nEnter your new PIN: ");
 								String newPin = scanner.next();
-								accountPin.remove(count);
+								//accountPin.remove(count);
 								accountPin.set(count, newPin);
-								System.out.print("\nNew PIN created successfully....");
+								System.out.print("\nNew PIN created successfully....\n");
 									}
 							else 
 							System.out.print("\nYou've entered a wrong PIN");
@@ -214,21 +287,30 @@ public static void main(String[] args){
 		
 			int index = accountNumber.indexOf(temporaryAccountNumber);
 				if(index != -1){
+
+			System.out.print("\nEnter your pin: \n");
+			String closePin = scanner.next();
+				int count = accountPin.indexOf(closePin);
+				if(count != -1){
 				accountBalance.remove(index);
 				accountPin.remove(index);
 				accountName.remove(index);
 				accountNumber.remove(index);
-				System.out.print("\nAccount closed successfully.\n");
+				System.out.print("\nAccount closed successfully.\n\n");
  				}	
-		
-				
+				else 
+				System.out.print("\nInvalid password.\n\n");
+				}
 		else
-		System.out.print("This "+temporaryAccountNumber+" does not exit in our database.\n Thanks for banking with us.");
+		System.out.print("This "+temporaryAccountNumber+" does not exit in our database.\nThanks for banking with us.\n\n");
 			
 			}
 
 
-	
+	System.out.println(accountNumber);
+	System.out.println(accountBalance);
+	System.out.println(accountPin);
+	System.out.println(accountName);
 
 
 
